3862663faa4fdfb527caf6e6645a80ffb78d6502
Dmitry Rakovich <dmitryrakovich@mail.ru>
2023-09-30 15:59:32 +0300

    add: routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage/HomePage'
+import ProjectPage from './pages/ProjectPage/ProjectPage'
 import './App.css'
 
 function App() {
-
   return (
     <>
-      hello
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/project/:id' element={<ProjectPage/>}/>
+      </Routes>
     </>
   )
 }
