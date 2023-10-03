61457d8e05ec718dbb6e2c50328dc2e601172f47
Dmitry Rakovich <dmitryrakovich@mail.ru>
2023-10-03 23:56:39 +0300

    fix

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 import dayjs from "dayjs"
-export const getDateDiff = (start:string, finish:string) => {
+export const getDateDiff = (start:string | Date, finish:string | Date) => {
     const days = dayjs(start).diff(finish, 'days')
     const months = dayjs(start).diff(finish, 'months')
     const years = dayjs(start).diff(finish, 'years')
