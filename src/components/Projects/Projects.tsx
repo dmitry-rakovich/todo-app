a3f916178a7885ea30bb62de5373687d50fda253
Dmitry Rakovich <dmitryrakovich@mail.ru>
2023-10-15 23:52:01 +0300

    fix: layout

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import { Project } from "../../types/DataTypes"
 import { useAppDispatch, useAppSelector } from "../../hooks/hooks"
-import { fetchProjects, addProject} from "../../redux/actions/projectActions"
+import { fetchProjects, addProject } from "../../redux/actions/projectActions"
 import styles from "./Projects.module.css"
 
 const Projects = () => {
@@ -13,7 +13,7 @@ const Projects = () => {
     const [isOpenForm, setIsOpenForm] = useState(false)
     const [value, setValue] = useState('')
 
-    const addNewProject = () => {        
+    const addNewProject = () => {
         dispatch(addProject({
             title: value,
             id: window.crypto.randomUUID()
@@ -34,35 +34,37 @@ const Projects = () => {
 
 
     return (
-        <div className={styles.projects}>
-            {isLoading ? <h1>Loading...</h1> : error ? <h1>{error}</h1> : <>
-            {
-                newProjects.map((project: Project) => (
-                    <div className={styles.project} key={project.id}>
-                        <Link to={`/project/${project.id}`}>
-                            <h1>{project.title}</h1>
-                        </Link>
-                    </div>
-                )
-                )
-            }
-            <div className={styles.project} onKeyUp={(e) => {
-                if(e.key === 'Enter' && value.trim()) addNewProject()
+        <>
+            <div className={styles.add} onKeyUp={(e) => {
+                if (e.key === 'Enter' && value.trim()) addNewProject()
             }}>
                 {
                     !isOpenForm
                         ? <button onClick={() => setIsOpenForm(true)}>Add new project</button>
-                        : <>
+                        : <div className={styles.form}>
                             <input type="text" value={value} onChange={(e) => setValue(e.target.value)} autoFocus placeholder="Add project title" />
                             <div className={styles.buttons}>
                                 <button onClick={addNewProject} disabled={!value.trim()}>Add</button>
                                 <button onClick={closeForm}>Close</button>
                             </div>
-                        </>
+                        </div>
                 }
             </div>
-            </>}
-        </div>
+            <div className={styles.projects}>
+                {isLoading ? <h1>Loading...</h1> : error ? <h1>{error}</h1> : <>
+                    {
+                        newProjects.map((project: Project) => (
+                            <div className={styles.project} key={project.id}>
+                                <Link to={`/project/${project.id}`}>
+                                    <h1>{project.title}</h1>
+                                </Link>
+                            </div>
+                        )
+                        )
+                    }
+                </>}
+            </div>
+        </>
     )
 }
 
