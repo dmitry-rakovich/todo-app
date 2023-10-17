bf541cbddd7fffeedea5d6f8aec2347b80ea023b
Dmitry Rakovich <dmitryrakovich@mail.ru>
2023-10-17 23:44:17 +0300

    test

--- src/components/SubtaskList/SubtaskList.tsx
+++ src/components/SubtaskList/SubtaskList.tsx
@@ -1,43 +1,43 @@
-import { Subtask } from "../../types/DataTypes"
-import SubtaskItem from "../SubtaskItem/SubtaskItem"
-import styles from "./Subtasklist.module.css"
-import { useAppDispatch } from "../../hooks/hooks"
-import { addSubTask } from "../../redux/actions/subtaskActions"
-import { useRef, useState } from "react"
-type Props = {
-    subtasks : Subtask[],
-    taskId: string
-}
-const SubtaskList = ({subtasks, taskId}: Props) => {
-    const dispatch = useAppDispatch()
-    const inputRef = useRef<HTMLInputElement>(null)
-    const [subTaskTitle, setSubTaskTitle] = useState('')
-    const addNewSubTask = () => {
-        dispatch(addSubTask({
-            id: window.crypto.randomUUID(),
-            checked: false,
-            taskId,
-            title: subTaskTitle
-        }))
-        setSubTaskTitle('')
-        inputRef.current?.focus()
-    }
+// import { Subtask } from "../../types/DataTypes"
+// // import SubtaskItem from "../SubtaskItem/SubtaskItem"
+// import styles from "./Subtasklist.module.css"
+// import { useAppDispatch } from "../../hooks/hooks"
+// // import { addSubTask } from "../../redux/actions/subtaskActions"
+// import { useRef, useState } from "react"
+// type Props = {
+//     subtasks : Subtask[],
+//     taskId: string
+// }
+// const SubtaskList = ({  taskId}: Props) => {
+//     const dispatch = useAppDispatch()
+//     const inputRef = useRef<HTMLInputElement>(null)
+//     const [subTaskTitle, setSubTaskTitle] = useState('')
+//     // const addNewSubTask = () => {
+//     //     dispatch(addSubTask({
+//     //         id: window.crypto.randomUUID(),
+//     //         checked: false,
+//     //         taskId,
+//     //         title: subTaskTitle
+//     //     }))
+//     //     setSubTaskTitle('')
+//     //     inputRef.current?.focus()
+//     // }
 
-    return (
-        <div className={styles.list}>
+//     return (
+//         <div className={styles.list}>
 
-            <h3>Subtasks</h3>
-            {
-                subtasks.map(subtask => <SubtaskItem key={subtask.id} subtask={subtask} />)
-            }
-            <div className={styles.form} onKeyUp={(e) => {
-                if (e.key === 'Enter' && subTaskTitle.trim()) addNewSubTask()
-            }}>
-                <input ref={inputRef} type="text" value={subTaskTitle} onChange={(e) => setSubTaskTitle(e.target.value)} placeholder="Add subtask" />
-                <button disabled={!subTaskTitle.trim()} onClick={addNewSubTask}>Add</button>
-            </div>
-        </div>
-    )
-}
+//             <h3>Subtasks</h3>
+//             {
+//                 // subtasks.map(subtask => <SubtaskItem key={subtask.id} subtask={subtask} />)
+//             }
+//             <div className={styles.form} onKeyUp={(e) => {
+//                 if (e.key === 'Enter' && subTaskTitle.trim()) addNewSubTask()
+//             }}>
+//                 <input ref={inputRef} type="text" value={subTaskTitle} onChange={(e) => setSubTaskTitle(e.target.value)} placeholder="Add subtask" />
+//                 <button disabled={!subTaskTitle.trim()} onClick={addNewSubTask}>Add</button>
+//             </div>
+//         </div>
+//     )
+// }
 
-export default SubtaskList
+// export default SubtaskList
--- src/redux/actions/subtaskActions.ts
+++ src/redux/actions/subtaskActions.ts
@@ -1,55 +1,55 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
-import { Subtask } from "../../types/DataTypes";
-import { subtaskActions } from "../slices/subtaskSlice";
-import { URL } from "../../constants";
+// import { createAsyncThunk } from "@reduxjs/toolkit";
+// import axios from "axios";
+// import { Subtask } from "../../types/DataTypes";
+// import { subtaskActions } from "../slices/subtaskSlice";
+// import { URL } from "../../constants";
 
-export const fetchSubTasks = createAsyncThunk(
-    'subtasks/fetchSubTasks',
-    async (id: string, thunkAPI) => {
-        try {
-            const response = await axios.get<Subtask[]>(`${URL}/subtasks?taskId=${id}`)
-            return response.data
-        } catch (error) {
-            return thunkAPI.rejectWithValue(error)
-        }
-    }
-)
+// export const fetchSubTasks = createAsyncThunk(
+//     'subtasks/fetchSubTasks',
+//     async (id: string, thunkAPI) => {
+//         try {
+//             const response = await axios.get<Subtask[]>(`${URL}/subtasks?taskId=${id}`)
+//             return response.data
+//         } catch (error) {
+//             return thunkAPI.rejectWithValue(error)
+//         }
+//     }
+// )
 
-export const addSubTask = createAsyncThunk('subtasks/addSubTask',
-    async (subtask: Subtask, thunkAPI) => {
-        try {
-            const response = await axios.post(`${URL}/subtasks`, subtask)
-            thunkAPI.dispatch(subtaskActions.addSubTask(response.data))
-            return response.data
-        } catch (error) {
-            return thunkAPI.rejectWithValue(error)
-        }
-    }
-)
+// export const addSubTask = createAsyncThunk('subtasks/addSubTask',
+//     async (subtask: Subtask, thunkAPI) => {
+//         try {
+//             const response = await axios.post(`${URL}/subtasks`, subtask)
+//             thunkAPI.dispatch(subtaskActions.addSubTask(response.data))
+//             return response.data
+//         } catch (error) {
+//             return thunkAPI.rejectWithValue(error)
+//         }
+//     }
+// )
 
-export const toggleSubTask = createAsyncThunk('subtasks/toggleSubTask',
-    async ({ id, checked }: { id: string, checked: boolean }, thunkAPI) => {
-        try {
-            const response = await axios.patch(`${URL}/subtasks/${id}`, {
-                checked
-            })
-            thunkAPI.dispatch(subtaskActions.editStatus({ id, checked }))
-            return response.data
-        } catch (error) {
-            return thunkAPI.rejectWithValue(error)
-        }
-    }
-)
+// export const toggleSubTask = createAsyncThunk('subtasks/toggleSubTask',
+//     async ({ id, checked }: { id: string, checked: boolean }, thunkAPI) => {
+//         try {
+//             const response = await axios.patch(`${URL}/subtasks/${id}`, {
+//                 checked
+//             })
+//             thunkAPI.dispatch(subtaskActions.editStatus({ id, checked }))
+//             return response.data
+//         } catch (error) {
+//             return thunkAPI.rejectWithValue(error)
+//         }
+//     }
+// )
 
-export const deleteSubTask = createAsyncThunk('subtasks/deleteSubTask',
-    async (id: string, thunkAPI) => {
-        try {
-            const response = await axios.delete(`${URL}/subtasks/${id}`)
-            thunkAPI.dispatch(subtaskActions.deleteSubTask(id))
-            return response.data
-        } catch (error) {
-            return thunkAPI.rejectWithValue(error)
-        }
-    }
-)
+// export const deleteSubTask = createAsyncThunk('subtasks/deleteSubTask',
+//     async (id: string, thunkAPI) => {
+//         try {
+//             const response = await axios.delete(`${URL}/subtasks/${id}`)
+//             thunkAPI.dispatch(subtaskActions.deleteSubTask(id))
+//             return response.data
+//         } catch (error) {
+//             return thunkAPI.rejectWithValue(error)
+//         }
+//     }
+// )
